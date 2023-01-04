5bd1643b9e730d51d8a26ec81898e33ec6e44c5f
PierreRtec <pierre.rondeau9pro@gmail.com>
2023-01-04 17:06:46 +0100

    FIX refactor project with "lettings" application + migrations OK

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Address, Letting, Profile
+from lettings.models import Letting, Address
+from .models import Profile
 
 admin.site.register(Letting)
 admin.site.register(Address)
