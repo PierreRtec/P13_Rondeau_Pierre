4b26fa8dd3e2f71c0d2d0e846374660364426f7c
PierreRtec <pierre.rondeau9pro@gmail.com>
2023-01-05 10:40:42 +0100

    FIX refactor lettings app

--- lettings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("lettings/", views.lettings_index, name="lettings_index"),
-    path("lettings/<int:letting_id>/", views.letting, name="letting"),
-]
