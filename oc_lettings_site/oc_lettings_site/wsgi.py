74a7c7c2ba3f591a484fa883c94e94059d68c47b
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-02 16:27:50 +0100

    FIX PORT in wsgi

--- oc_lettings_site/oc_lettings_site/wsgi.py
+++ oc_lettings_site/oc_lettings_site/wsgi.py
@@ -1,8 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("PORT", "8080")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
-
 application = get_wsgi_application()
+
+port = os.environ.get('PORT')
+if port:
+    application.run(port=int(port))
