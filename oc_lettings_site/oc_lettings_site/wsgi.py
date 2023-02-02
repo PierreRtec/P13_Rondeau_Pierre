38970940a4c10184c8b9e92d551b6837292aab03
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-02 16:31:14 +0100

    FIX PORT in wsgi

--- oc_lettings_site/oc_lettings_site/wsgi.py
+++ oc_lettings_site/oc_lettings_site/wsgi.py
@@ -5,5 +5,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
 application = get_wsgi_application()
 
 port = os.environ.get('PORT')
-if port:
-    application.run(port=int(port))
+
