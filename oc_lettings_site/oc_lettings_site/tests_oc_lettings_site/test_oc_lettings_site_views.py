ebb8e95cacbcd86d26227565f93d068d684b5ff8
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-15 12:10:08 +0100

    FIX tests paths

--- oc_lettings_site/oc_lettings_site/tests_oc_lettings_site/test_oc_lettings_site_views.py
+++ oc_lettings_site/oc_lettings_site/tests_oc_lettings_site/test_oc_lettings_site_views.py
@@ -2,7 +2,9 @@ from assertpy import assert_that
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
-from .models import Address, Letting, Profile
+
+from oc_lettings_site.lettings.models import Address, Letting
+from oc_lettings_site.profiles.models import Profile
 
 client = Client()
 
