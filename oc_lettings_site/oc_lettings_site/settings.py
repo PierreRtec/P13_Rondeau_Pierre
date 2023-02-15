ac0a9ad7486182d8e57a9a47775ed7d172c0ada4
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-15 12:31:19 +0100

    FIX reset to fix paths django apps

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -20,9 +20,9 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "lettings.apps.LettingsConfig",
-    "profiles.apps.ProfilesConfig",
-    "oc_lettings_site.apps.OCLettingsSiteConfig",
+    "oc_lettings_site.lettings.apps.LettingsConfig",
+    "oc_lettings_site.profiles.apps.ProfilesConfig",
+    # "oc_lettings_site.apps.OCLettingsSiteConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "oc_lettings_site.urls"
+ROOT_URLCONF = "oc_lettings_site.oc_lettings_site.urls"
 
 TEMPLATES = [
     {
