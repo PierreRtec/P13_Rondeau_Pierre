68a7ec2e8d7d06739960860c4541babd99c7106a
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-17 13:21:09 +0100

    FIX set debug mode + fix apps import ordre + fix TEMPLATES DIRS

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -13,22 +13,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    "oc_lettings_site.lettings.apps.LettingsConfig",
-    "oc_lettings_site.profiles.apps.ProfilesConfig",
-    # "oc_lettings_site.apps.OCLettingsSiteConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # oc_lettings_site apps
+    "oc_lettings_site.lettings",
+    "oc_lettings_site.profiles",
 ]
 
 MIDDLEWARE = [
@@ -46,7 +46,7 @@ ROOT_URLCONF = "oc_lettings_site.oc_lettings_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "profiles")],
+        "DIRS": [os.path.join(BASE_DIR, "oc_lettings_site", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
