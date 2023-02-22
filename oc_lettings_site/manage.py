b9a1760d8de1269851b86179f1305225af565558
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2023-02-22 12:12:10 +0100

    FIX path regex antislash

--- oc_lettings_site/manage.py
+++ oc_lettings_site/manage.py
@@ -6,7 +6,7 @@ import sys
 def main():
     path = (
         "oc_lettings_site."
-        if re.split(r"[/\\]", os.getcwd())[-1] in ["P13_Rondeau_Pierre", "project"]
+        if re.split(r"[\\\\/]", os.getcwd())[-1] in ["P13_Rondeau_Pierre", "project"]
         else ""
     )
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", path + "oc_lettings_site.settings")
